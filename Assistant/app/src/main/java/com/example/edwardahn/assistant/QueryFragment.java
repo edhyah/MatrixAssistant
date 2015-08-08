0c3171fff2d7dc223dc82008776edea8d045576d
Edward Ahn <edhyah@gmail.com>
2015-08-08 23:05:47 +0900

    keyboard disappears after input in query mode

--- Assistant/app/src/main/java/com/example/edwardahn/assistant/QueryFragment.java
+++ Assistant/app/src/main/java/com/example/edwardahn/assistant/QueryFragment.java
@@ -15,6 +15,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -99,7 +100,7 @@ public class QueryFragment extends Fragment implements View.OnClickListener {
         //mConversationView.setAdapter(mConversationArrayAdapter);
 
         // Initialize the compose field with a listener for the return key
-        mOutEditText.setOnEditorActionListener(mWriteListener);
+        //mOutEditText.setOnEditorActionListener(mWriteListener);
 
         // Initialize the send button with a listener that for click events
         mSendButton.setOnClickListener(new View.OnClickListener() {
@@ -110,29 +111,24 @@ public class QueryFragment extends Fragment implements View.OnClickListener {
                     TextView textView = (TextView) view.findViewById(R.id.edit_text_out);
                     String message = textView.getText().toString();
                     if (message.equals("")) return;
+
                     textView.setText("");
                     TextView queryView = (TextView) view.findViewById(R.id.query_in);
                     queryView.setText(message);
+
+                    InputMethodManager inputManager = (InputMethodManager) getActivity().
+                            getSystemService(Context.INPUT_METHOD_SERVICE);
+                    inputManager.hideSoftInputFromWindow(mOutEditText.getWindowToken(),
+                          InputMethodManager.HIDE_NOT_ALWAYS);
+
                     new QAThread(message).start();
-                    //((MainActivity) getActivity()).sendMessage(label+message+label);
                 }
             }
         });
     }
 
-    // The action listener for the EditText widget, to listen for the return key
-    private TextView.OnEditorActionListener mWriteListener
-            = new TextView.OnEditorActionListener() {
-        public boolean onEditorAction(TextView view, int actionId, KeyEvent event) {
-            // If the action is a key-up event on the return key, send the message
-            if (actionId == EditorInfo.IME_NULL && event.getAction() == KeyEvent.ACTION_UP) {
-                
-                String message = view.getText().toString();
-                ((MainActivity) getActivity()).sendMessage(label+message+label);
-            }
-            return true;
-        }
-    };
+    //TODO: time displayed incorrectly, need to possibly flush buffer
+    
 
     class QAThread extends Thread {
         String inputText;
